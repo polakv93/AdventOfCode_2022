948761fd5ace67f4de6ca073c2e86affed6b0e31
polakv93 <polakpolakiewicz@gmail.com>
2022-12-03 00:00:11 +0100

    day02

---
+++ AdventOfCode_2022/Day02/Program.cs
@@ -0,0 +1,35 @@
+﻿var scores = new Dictionary<char, int>
+{
+    { 'A', 1 },
+    { 'B', 2 },
+    { 'C', 3 }
+};
+
+var charShift = 'X' - 'A';
+
+var score = 0;
+const char rock = 'A';
+const char paper = 'B';
+const char scissors = 'C';
+
+foreach (var line in File.ReadLines("./input.txt"))
+{
+    var opponent = line[0];
+    var my = (char)(line[2] - charShift);
+    score += scores[my];
+
+    if (my == opponent)
+    {
+        score += 3;
+        continue;
+    }
+    if ((my == rock && opponent == scissors )||
+        (my == scissors && opponent == paper )||
+        (my == paper && opponent == rock))
+    {
+        score += 6;
+        continue;
+    }
+}
+
+Console.WriteLine(score);
