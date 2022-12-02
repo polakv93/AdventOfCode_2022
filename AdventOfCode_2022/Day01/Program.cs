2962f90428263af368a33e336c8828f77afa84aa
polakv93 <polakpolakiewicz@gmail.com>
2022-12-02 23:20:47 +0100

    day01

---
+++ AdventOfCode_2022/Day01/Program.cs
@@ -0,0 +1,20 @@
+﻿var maxCalories = 0;
+var sumOfCurrentCalories = 0;
+foreach (var line in File.ReadLines("./input.txt"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if (sumOfCurrentCalories > maxCalories)
+        {
+            maxCalories = sumOfCurrentCalories;
+        }
+
+        sumOfCurrentCalories = 0;
+    }
+    else
+    {
+        sumOfCurrentCalories += int.Parse(line);
+    }
+}
+
+Console.WriteLine(maxCalories);
