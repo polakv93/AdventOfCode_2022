6a9fb65604e13014860e127fa1d950197798d913
polakv93 <polakpolakiewicz@gmail.com>
2022-12-04 10:55:47 +0100

    day04 part 1

---
+++ AdventOfCode_2022/Day04/Program.cs
@@ -0,0 +1,23 @@
+﻿var overlappingPairs = 0;
+foreach (var pairs in File.ReadLines("./input.txt"))
+{
+    var groups = pairs.Split(',');
+    var firstPair = groups[0].Split('-').Select(int.Parse).ToArray();
+    var secondPair = groups[1].Split('-').Select(int.Parse).ToArray();
+    
+    //  f[0]|-------|f[1]
+    //    s[0]|---|s[1]
+    
+    //   f[0]|-|f[1]
+    //  s[0]|---|s[1]
+
+    if ((firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[1]) ||
+        (secondPair[0] <= firstPair[0] && secondPair[1] >= firstPair[1]))
+    {
+        overlappingPairs++;
+    }
+    
+    
+}
+
+Console.WriteLine(overlappingPairs);
