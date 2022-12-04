a1d1951f16c85f18366eb3a22f706b949d6cb51b
polakv93 <polakpolakiewicz@gmail.com>
2022-12-04 10:21:48 +0100

    day03 part 1

---
+++ AdventOfCode_2022/Day03/Program.cs
@@ -0,0 +1,36 @@
+﻿var priorities = new Dictionary<char, int>();
+
+var i = 'a';
+var priority = 1;
+while (i <= 'z')
+{
+    priorities.Add(i, priority);
+    priority++;
+    i++;
+}
+
+i = 'A';
+while (i <= 'Z')
+{
+    priorities.Add(i, priority);
+    priority++;
+    i++;
+}
+
+var score = 0;
+
+
+foreach (var line in File.ReadLines("./input.txt"))
+{
+    var mid = line.Length / 2;
+    var first = line.Take(mid);
+    var second = line.Skip(mid);
+
+    var intersected = first.Intersect(second);
+    foreach (var c in intersected)
+    {
+        score += priorities[c];
+    }
+}
+
+Console.WriteLine(score);
